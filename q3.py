327b3af138533189615d21b626af6f1f3471d0a5
go0leum <mijin575@konkuk.ac.kr>
2024-10-03 21:48:43 +0900

    update:q3.py

--- q3.py
+++ q3.py
@@ -1,9 +1,9 @@
 import gymnasium as gym
 from policyiteration import PolicyIteration
 
-# policy iteration 1번 반복
+# policy iteration 1번,policy improvement 1번을 10번 반복
 print("======iter:1======")
-N = 100
+N = 10
 # environment 생성
 env = gym.make('Taxi-v3', render_mode="ansi")
 # policy iteration 생성 및 설정
@@ -18,9 +18,13 @@ for n in range(N):
 
     policyIteration.print_value()
 
-# policy iteration 10번 반복
+# policy iteration 결과 출력
+print("3. print result")
+policyIteration.print_result()
+
+# policy iteration 10번, policy improvement 1번
 print("======iter:10======")
-N = 100
+N = 10
 # environment 생성
 env = gym.make('Taxi-v3', render_mode="ansi")
 # policy iteration 생성 및 설정
@@ -30,7 +34,10 @@ for n in range(N):
     print("{}-1. Policy Evaluation".format(n+1))
     policyIteration.policy_evaluation(iter_num=10)
 
-    print("{}-2. Policy improvement".format(n+1))
-    policyIteration.policy_improvement()
-
     policyIteration.print_value()
+
+print("{}-2. Policy improvement".format(n+1))
+policyIteration.policy_improvement()
+# policy iteration 결과 출력
+print("3. print result")
+policyIteration.print_result()
